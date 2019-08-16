11166d0ea8b9129aa2999ef52b6e568f8d21c366
Dan Nielsen <saattrupdan@gmail.com>
2019-08-17 00:51:07 +0100

    added better pool selection, parallelism and tqdm progress bar

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,85 +1,185 @@
+# Core packages
 import numpy as np
+from functools import reduce
+import sys
+import os
+
+# Progress bar
 from tqdm import tqdm
+
+# Parallelising fitness
+from multiprocessing import Pool
+
+# Used to suppress console output
+from contextlib import contextmanager
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
 
 class Genus():
 
-    def __init__(self, dna_range):
-        self.dna_range = dna_range
+    def __init__(self, genomes: dict):
+        self.genomes = genomes
 
-    def create_organisms(self, amount = 1):
-        organisms = [Organism(genus = self, dna =
-            {key : np.random.choice(self.dna_range[key])
-            for key in self.dna_range.keys()}) for i in range(amount)]
+    def create_organisms(self, amount: int = 1):
+        ''' Create organisms of this genus. '''
+
+        organisms = [Organism(genus = self, genome =
+            {key : np.random.choice(self.genomes[key])
+            for key in self.genomes.keys()}) for i in range(amount)]
         return np.array(organisms)
 
     def create_genus(self, add = None, remove = None):
-        dna_range = np.append(np.array([param for param in self.dna_range
+        ''' Create copy of this genus with possible modifications. '''
+        genomes = np.append(np.array([param for param in self.genomes
                      if param.key not in remove]), add)
-        return Genus(dna_range)
+        return Genus(genomes)
 
 class Organism():
 
-    def __init__(self, genus: Genus, dna: dict):
-        if not genus.dna_range.keys() == dna.keys():
+    def __init__(self, genus: Genus, genome: dict):
+        if not genus.genomes.keys() == genome.keys():
             raise TypeError('Genus keys do not match input keys.')
+
         self.genus = genus
-        self.dna = dna
+        self.genome = genome
+        self.fitness = None
 
     def breed(self, other):
+        ''' Breed with another organism, creating a new organism of
+            the same genus. '''
+
         if not self.genus == other.genus:
             raise TypeError('The two organisms are not of the same genus.')
-        child_dna = {
-            key : np.random.choice([self.dna[key], other.dna[key]])
-                  for key in self.dna.keys()
+
+        # Child will inherit genes from its parents randomly
+        child_genome = {
+            key : np.random.choice([self.genome[key], other.genome[key]])
+                  for key in self.genome.keys()
             }
-        return Organism(self.genus, child_dna)
+        return Organism(self.genus, child_genome)
 
     def mutate(self):
-        keys = np.array([*self.genus.dna_range])
+        ''' Mutate the organism by changing the value of a random gene. '''
+        keys = np.array([*self.genus.genomes])
         random_key = np.random.choice(keys)
-        random_val = np.random.choice(self.genus.dna_range[random_key])
-        self.dna[random_key] = random_val
+        random_val = np.random.choice(self.genus.genomes[random_key])
+        self.genome[random_key] = random_val
+        self.fitness = None
         return self
 
 class Population():
 
-    def __init__(self, genus, size, fitness_fn):
+    def __init__(self, genus: Genus, size: int, fitness_fn):
         self.genus = genus
         self.size = size
-        self.fitness_fn = fitness_fn
         self.population = self.genus.create_organisms(self.size)
+        self.most_fit = None
 
-    def get_fittest(self, top_n = 1):
-        fitnesses = np.array([self.fitness_fn(organism)
-                              for organism in self.population])
-        fittest_idx = np.argpartition(fitnesses, -top_n)[-top_n:]
-        fittest_organisms = self.population[fittest_idx]
-        return fittest_organisms
+        # Fitness function cannot be a lambda expression
+        self.fitness_fn = fitness_fn
 
-    def evolve(self, generations = 1, keep = 0.30, mutate = 0.10):
+    def get_fit_organisms(self, amount: int = 1):
+        ''' Sample a fixed amount of organisms from the population,
+            where the fitter an organism is, the more it's likely
+            to be chosen. 
+    
+        INPUT
+            (int) amount: number of fit organisms to output
+
+        OUTPUT
+            (ndarray) fit subset of population
+        '''
+
+        # Get array of organisms in population with no fitness recorded
+        pop_no_fit = np.asarray([org for org in self.population
+                                 if not org.fitness])
+
+        # Compute fitness values in parallel
+        with Pool() as pool:
+            try:
+                new_fitnesses = pool.map(self.fitness_fn, pop_no_fit)
+            except:
+                raise RuntimeError('Your fitness function is not ' \
+                   'pickleable. Try defining your function using ' \
+                   '"def" rather than using lambda expressions.')
+
+        # Assign the new fitness values to the population
+        for (org, new_fitness) in zip(pop_no_fit, new_fitnesses):
+            org.fitness = new_fitness
+        fitnesses = np.asarray([org.fitness for org in self.population])
+        
+        # Convert fitness values into probabilities
+        probs = np.divide(fitnesses, sum(fitnesses))
+
+        # Sort the probabilities in descending order and sort the
+        # population and fitnesses in the same way
+        sorted_idx = np.argsort(probs)[::-1]
+        probs = probs[sorted_idx]
+        self.population = self.population[sorted_idx]
+
+        # Save the most fit genome with its fitness value
+        self.most_fit = (self.population[0].genome, fitnesses[sorted_idx[0]])
+       
+        # Get random numbers between 0 and 1 
+        indices = np.random.rand(amount)
+        for i in range(amount):
+            # Find the index of the fitness value whose accumulated
+            # sum exceeds the value of the i'th random number.
+            fn = lambda x, y: (x[0], x[1] + y[1]) \
+                              if x[1] + y[1] > indices[i] \
+                              else (x[0] + y[0], x[1] + y[1])
+            (idx, _) = reduce(fn, map(lambda x: (1, x), probs))
+            indices[i] = idx - 1
+
+        # Return the organisms indexed at the indices found above
+        return self.population[indices.astype(int)]
+
+    def evolve(self, generations: int = 1, keep: float = 0.20,
+        mutate: float = 0.50):
+        ''' Evolve the population.
+
+        INPUTS
+            (int) generations: number of generations to evolve
+            (float) keep: percentage of population to keep each gen
+            (float) mutate: percentage of population to mutate each gen
+        '''
+    
+        print("Population evolving...")
         for generation in tqdm(range(generations)):
-            # keep the fittest organisms of the population
-            keep_amount = max(2, np.ceil(self.size * keep).astype(int))
-            self.population = self.get_fittest(keep_amount)
+            with suppress_stdout():
+                # Keep the fittest organisms of the population
+                keep_amount = max(2, np.ceil(self.size * keep).astype(int))
+                self.population = self.get_fit_organisms(keep_amount)
+           
+                # Breed until we reach the same size
+                remaining = self.size - keep_amount
+                for i in range(remaining):
+                    parents = np.random.choice(self.population, 2)
+                    child = parents[0].breed(parents[1])
+                    
+                    # Mutate child
+                    if np.random.rand() < mutate: child.mutate()
+                    
+                    # Add child to population
+                    self.population = np.append(self.population, child)
 
-            # breed until we reach the same size
-            remaining = self.size - keep_amount
-            for i in range(remaining):
-                organisms = np.random.choice(self.population, 2)
-                new_organism = organisms[0].breed(organisms[1])
-                self.population = np.append(self.population, new_organism)
-
-            # mutate randomly
-            mutate_amount = np.ceil(self.size * mutate).astype(int)
-            mutators = np.random.choice(self.population, mutate_amount)
-            for mutator in mutators:
-                mutator.mutate()
         return self
 
 
 if __name__ == '__main__':
-    Number = Genus({'val' : range(0, 100000)})
-    fn = lambda number: number.dna['val']
-    numbers = Population(genus = Number, size = 3, fitness_fn = fn)
-    numbers.evolve(generations = 500)
-    print(numbers.get_fittest()[0].dna['val'])
+    Number = Genus({'x' : range(1, 10000), 'y' : range(1, 10000)})
+    def fn(number):
+        return number.genome['x'] / number.genome['y']
+    numbers = Population(genus = Number, size = 20, fitness_fn = fn)
+    numbers.evolve(generations = 200)
+    print(f"Most fit organism:")
+    print(f"\tGenome: {numbers.most_fit[0]}")
+    print(f"\tFitness: {np.around(numbers.most_fit[1], 2)}")
