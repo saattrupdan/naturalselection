874d70bfedc404a630ed4f6d9b6a98783316f95f
Dan Nielsen <saattrupdan@gmail.com>
2019-09-01 11:48:31 +0100

    cleaned up mnist and cifar10 examples a bit

--- cifar10_example.py
+++ cifar10_example.py
@@ -2,45 +2,34 @@ import naturalselection as ns
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import cifar10
 
-(X_train, Y_train), (X_val, Y_val) = cifar10.load_data()
+def cifar10_preprocessing(X):
+    ''' Basic normalisation and scaling preprocessing. '''
+    X = X.reshape((-1, 3072))
+    X = (X - X.min()) / (X.max() - X.min())
+    X -= X.mean(axis = 0)
+    return X
 
-X_train = ((X_train / 255) - 0.5).reshape((-1, 3072))
+(X_train, Y_train), (X_val, Y_val) = cifar10.load_data()
+X_train = cifar10_preprocessing(X_train)
 Y_train = to_categorical(Y_train)
-X_val = ((X_val / 255) - 0.5).reshape((-1, 3072))
+X_val = cifar10_preprocessing(X_val)
 Y_val = to_categorical(Y_val)
 
 fnns = ns.FNNs(
-    size = 20,
+    size = 50,
     train_val_sets = (X_train, Y_train, X_val, Y_val),
     loss_fn = 'categorical_crossentropy',
     score = 'accuracy',
     output_activation = 'softmax',
-    patience = 5,
-    min_change = 0,
-    neurons = [64, 128, 256, 512, 768, 1024],
-    uniform_layers = True,
-    max_number_of_hidden_layers = 3,
-    hidden_activation = ['relu', 'elu', 'tanh', 'sigmoid'],
-    optimizer = ['rmsprop', 'adam', 'sgd', 'adagrad', 'adadelta',
-                  'adamax', 'nadam'],
-    input_dropout = [0.2],
-    hidden_dropout = [0.2],
-    batch_size = [64],
-    initializer = ['glorot_uniform'],
+    max_training_time = 60,
     )
 
-history = fnns.evolve(
-    generations = 10, 
-    multiprocessing = False,
-    verbose = 2
-    )
+history = fnns.evolve(generations = 20)
 print("Best overall genome:", history.fittest)
 
 history.plot(
     title = "Validation accuracy by generation",
     ylabel = "Validation accuracy",
-    show_plot = False,
-    file_name = "cifar10_plot.png"
     )
 
 best_score = fnns.train_best()
--- mnist_example.py
+++ mnist_example.py
@@ -2,9 +2,16 @@ import naturalselection as ns
 from tensorflow.keras.utils import to_categorical
 import mnist
 
-X_train = ((mnist.train_images() / 255) - 0.5).reshape((-1, 784))
+def mnist_preprocessing(X):
+    ''' Basic normalisation and scaling preprocessing. '''
+    X = X.reshape((-1, 784))
+    X = (X - X.min()) / (X.max() - X.min())
+    X -= X.mean(axis = 0)
+    return X
+
+X_train = mnist_preprocessing(mnist.train_images())
 Y_train = to_categorical(mnist.train_labels())
-X_val = ((mnist.test_images() / 255) - 0.5).reshape((-1, 784))
+X_val = mnist_preprocessing(mnist.test_images())
 Y_val = to_categorical(mnist.test_labels())
 
 fnns = ns.FNNs(
@@ -22,8 +29,6 @@ print("Best overall genome:", history.fittest)
 history.plot(
     title = "Validation accuracy by generation",
     ylabel = "Validation accuracy",
-    show_plot = False,
-    file_name = "mnist_plot.png"
     )
 
 best_score = fnns.train_best()
