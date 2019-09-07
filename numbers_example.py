0c77489b5d95ca2504dbf31c873d4f5f3b4f1545
Dan Nielsen <saattrupdan@gmail.com>
2019-09-07 15:12:44 +0100

    changed numbers example a tiny bit

--- numbers_example.py
+++ numbers_example.py
@@ -5,8 +5,7 @@ Pair = ns.Genus(x = range(1, 10000), y = range(1, 10000))
 pairs = ns.Population(
     genus = Pair, 
     size = 100, 
-    fitness_fn = lambda n: n.x/n.y,
-    initial_genome = {'x': 9750, 'y': 15}
+    fitness_fn = lambda n: n.x/n.y
     )
 
 history = pairs.evolve(generations = 100)
